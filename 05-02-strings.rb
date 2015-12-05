b674804661f3221a2c1f2eb1f63769aec7ad89b0
Merrin <merrin.macleod@gmail.com>
2015-12-05 19:24:00 +1300

    day five

--- 05-02-strings.rb
+++ 05-02-strings.rb
@@ -1,7 +1,7 @@
 strings = File.read('05-strings.txt').split("\n")
 nice_strings = 0
 strings.each do |s|
-  if s =~ /(\w)\1+/ && s =~ /.*[aeiou].*[aeiou].*[aeiou].*/ && s !~ /.*ab|cd|pq|xy.*/
+  if s =~ /(\w{2}).*\1+/ && s =~ /(\w).\1/
     nice_strings += 1
   end
 end
