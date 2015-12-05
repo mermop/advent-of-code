c49e0c2ee3d6735788ad8d008fb5810c4b380b85
Merrin <merrin.macleod@gmail.com>
2015-12-05 18:30:38 +1300

    initial try

--- 01-02-basement-santa.rb
+++ 01-02-basement-santa.rb
@@ -1,8 +1,7 @@
 f = File.read('01-santa.txt')
-movements = f.split("")
 position = 0
 movement = 0
-movements.each do |m|
+f.each_char do |m|
   unless position == -1
     case m
       when "("
@@ -10,7 +9,6 @@ movements.each do |m|
       when ")"
         position -= 1
     end
-    puts position
     movement += 1
   end
 end
---
+++ 05-01-strings.rb
@@ -0,0 +1,58 @@
+# It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+# It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+# It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+
+class Santastring
+  def initialize(s)
+    @s = s
+    @niceness = false
+  end
+  def naughty_or_nice do
+    case @s
+    when /.*ab|cd|pq|xy.*/
+      @niceness = false
+    when /.*cd.*/
+      @niceness = false
+    when /.*pq.*/
+      @niceness = false
+    when /.*xy.*/
+      @niceness = false
+    when /.*[aeiou].*[aeiou].*[aeiou].*/
+      @niceness = true
+    when /.{2}/
+      @niceness = true
+    end
+  end
+end
+
+strings = File.read('05-strings.txt')
+strings = strings.split("\n")
+strings.each do |s|
+  Santastring.new(s)
+end
+naughty_strings = 0
+nice_strings = 0
+
+strings.each do |s|
+  if s.include?('ab')
+    naughty_strings += 1
+  end
+  if s.include?('cd')
+    naughty_strings += 1
+  end
+  if s.include?('pq')
+    naughty_strings += 1
+  end
+  if s.include?('xy')
+    naughty_strings += 1
+  end
+  if s =~ /[aeiou].*[aeiou].*[aeiou]/
+    nice_strings += 1
+  end
+  if s =~ /.{2}/
+    nice_strings += 1
+  end
+end
+
+puts "Naughty: #{naughty_strings}"
+puts "Nice: #{nice_strings}"
