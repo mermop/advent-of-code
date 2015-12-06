c14a56fc57b38d2fa8e755c98ddf5e4b287d59c1
Merrin <merrin.macleod@gmail.com>
2015-12-06 19:08:40 +1300

    it is useful to see the instructions while it is running

--- ruby/06.rb
+++ ruby/06.rb
@@ -1,19 +1,3 @@
-# Because your neighbors keep defeating you in the holiday house decorating contest year after year, you've decided to deploy one million lights in a 1000x1000 grid.
-#
-# Furthermore, because you've been especially nice this year, Santa has mailed you instructions on how to display the ideal lighting configuration.
-#
-# Lights in your grid are numbered from 0 to 999 in each direction; the lights at each corner are at 0,0, 0,999, 999,999, and 999,0. The instructions include whether to turn on, turn off, or toggle various inclusive ranges given as coordinate pairs. Each coordinate pair represents opposite corners of a rectangle, inclusive; a coordinate pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square. The lights all start turned off.
-#
-# To defeat your neighbors this year, all you have to do is set up your lights by doing the instructions Santa sent you in order.
-#
-# For example:
-#
-# turn on 0,0 through 999,999 would turn on (or leave on) every light.
-# toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the ones that were on, and turning on the ones that were off.
-# turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
-# After following the instructions, how many lights are lit?
-#
-
 instructions = File.read('../data/06.txt').split("\n")
 
 grid = [[]]
@@ -26,6 +10,7 @@ for i in 0..1000 do
 end
 
 instructions.each do |i|
+  puts i
   if i.start_with?('turn on')
     first_light = i.split('turn on ')[-1].split(' through ')[0].split(',')
     last_light = i.split('turn on ')[-1].split(' through ')[-1].split(',')
@@ -63,6 +48,3 @@ grid.each do |g|
 end
 
 puts lights_lit
-# turn on
-# turn off
-# toggle
